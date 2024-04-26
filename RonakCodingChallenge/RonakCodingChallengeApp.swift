b1c1458e53cc62ff7790b6759660ccefab096c51
vino <vinodhkumarg@ymail.com>
2024-04-27 03:07:47 +0530

    Initial Commit

---
+++ RonakCodingChallenge/RonakCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RonakCodingChallengeApp.swift
+//  RonakCodingChallenge
+//
+//  Created by vinodh kumar on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct RonakCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
